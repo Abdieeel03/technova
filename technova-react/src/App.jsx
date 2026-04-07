a8e0a9865391bac4426e0208c720a8d3aa76b6cb
Abdieeel03 <marcelopostigo54@gmail.com>
2026-04-07 17:43:05 -0500

    feat: Se hizo la configuración inicial para React Router

--- technova-react/src/App.jsx
+++ technova-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
+import { Routes, Route } from "react-router";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,9 @@ function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
       <Footer />
     </>
   );
