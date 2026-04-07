f52bd44be42c29e1b5c33e6a86be3a4132972641
Abdieeel03 <marcelopostigo54@gmail.com>
2026-04-07 16:47:35 -0500

    feat: Implementacion del header y sus estilos
    Se implementaron los archivos Header.jsx y Header.module.css, además se eliminaron archivos css por defecto para resolver conflictos

---
+++ technova-react/src/components/ui/Header.jsx
@@ -0,0 +1,31 @@
+import styles from "../../css_components/Header.module.css";
+import logo from "../../assets/img/logo.svg";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <a className={styles.headerLink} href="./index.html">
+          <img className={styles.logo} src={logo} alt="Logo" />
+          <h1 className={styles.headerTitle}>TechNova</h1>
+        </a>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="./index.html">Inicio</a>
+          </li>
+          <li>
+            <a href="./pages/productos.html">Productos</a>
+          </li>
+          <li>
+            <a href="./pages/nosotros.html">Nosotros</a>
+          </li>
+          <li>
+            <a href="./pages/contacto.html">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
