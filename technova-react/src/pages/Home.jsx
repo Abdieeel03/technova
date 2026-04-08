73f005b71a72e86b429590b27389343d09e8571a
César Suazo <suazoromerojamil@gmail.com>
2026-04-08 18:06:23 -0500

    feat: añadiendo rutas de productos destacados

--- technova-react/src/pages/Home.jsx
+++ technova-react/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/ui/Hero";
+import FeaturedProducts from "../components/ui/FeaturedProducts";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 }
