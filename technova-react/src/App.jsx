6a42a571c9aa71a80ce28ad06b3a98a1b1c94de1
pinkidu14 <tinocomauryleslie@gmail.com>
2026-04-10 12:23:20 -0500

    Feat: Añadí la ruta

--- technova-react/src/App.jsx
+++ technova-react/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import Nosotros from "./pages/Nosotros";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/nosotros" element={<Nosotros />} />
       </Routes>
       <Footer />
     </>
