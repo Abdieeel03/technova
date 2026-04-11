5f85c7d7ff6ba2135c91a8213089e148d619ef3c
PieroR <pierordv@gmail.com>
2026-04-10 18:08:40 -0600

    agregar ruta de productos

--- technova-react/src/App.jsx
+++ technova-react/src/App.jsx
@@ -1,9 +1,10 @@
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Nosotros from "./pages/Nosotros";
 import Contacto from "./pages/Contacto";
+import Productos from "./pages/Productos";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
         <Route path="/nosotros" element={<Nosotros />} />
         <Route path="/contacto" element={<Contacto />} />
       </Routes>
