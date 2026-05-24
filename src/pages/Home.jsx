c7b76999d1a46641c52870552961a847670d8cd9
César Suazo <suazoromerojamil@gmail.com>
2026-05-24 05:01:18 -0500

    feat: añadiendo carrusel de marcas a la página principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/ui/Hero";
 import PromoCarousel from "../components/ui/PromoCarousel";
 import FeaturedProducts from "../components/ui/FeaturedProducts";
+import BrandsCarousel from "../components/ui/BrandsCarousel";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <BrandsCarousel />
       <PromoCarousel />
     </>
   );
