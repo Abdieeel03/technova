e53c5fd9201a26ee0aefdc4362e6553890c37a25
César Suazo <suazoromerojamil@gmail.com>
2026-05-24 13:51:14 -0500

    feat: agregando "más vendidos" a la pag principal - error: no renderiza y no guardas el productos.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/ui/Hero";
 import PromoCarousel from "../components/ui/PromoCarousel";
 import FeaturedProducts from "../components/ui/FeaturedProducts";
+import BestSellers from "../components/ui/BestSellers";
 import BrandsCarousel from "../components/ui/BrandsCarousel";
 import CategoryCarousel from "../components/ui/CategoryCarousel";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <Hero />
       <FeaturedProducts />
+      <BestSellers />
       <CategoryCarousel tipo="pantalla" titulo="🖥️ Pantallas" />
       <CategoryCarousel tipo="mouse" titulo="🖱️ Mouse" />
       <CategoryCarousel tipo="audífonos" titulo="🎧 Audífonos" />
