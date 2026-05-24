fbd3250c17935fdfa189a94230882bfd1560d23f
César Suazo <suazoromerojamil@gmail.com>
2026-05-24 13:37:40 -0500

    feat: agregando carrusel de productos a la pag principal - error: no se rederiza sin guardar  products.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import Hero from "../components/ui/Hero";
 import PromoCarousel from "../components/ui/PromoCarousel";
 import FeaturedProducts from "../components/ui/FeaturedProducts";
 import BrandsCarousel from "../components/ui/BrandsCarousel";
+import CategoryCarousel from "../components/ui/CategoryCarousel";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <CategoryCarousel tipo="pantalla" titulo="🖥️ Pantallas" />
+      <CategoryCarousel tipo="mouse" titulo="🖱️ Mouse" />
+      <CategoryCarousel tipo="audífonos" titulo="🎧 Audífonos" />
       <BrandsCarousel />
       <PromoCarousel />
     </>
